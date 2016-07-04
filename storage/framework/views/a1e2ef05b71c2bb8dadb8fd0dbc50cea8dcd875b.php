bd8155664aa6745ddfcd54ffd772d38ef829fe46
Gregory House <theemfs@gmail.com>
2016-07-04 13:59:54 +0800

    cleaning

--- storage/framework/views/a1e2ef05b71c2bb8dadb8fd0dbc50cea8dcd875b.php
+++
@@ -1,8 +0,0 @@
-<!-- JavaScripts -->
-<script src="<?php echo e(elixir('js/all.js')); ?>"></script>
-
-<?php echo $__env->yieldContent('footer'); ?>
-
-<footer class="footer">
-	<p class="text-muted">&copy</p>
-</footer>
