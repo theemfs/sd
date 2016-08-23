a49effbf6150d50a5012c6d6b69b323887e31fa2
theemfs <theemfs@gmail.com>
2016-08-23 14:35:40 +0800

    some fixes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,29 +10,19 @@
 				<form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
 					{!! csrf_field() !!}
 
-					<div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+					<div class="form-group">
 						<label class="col-md-3 control-label">{{ trans('app.Email') }}</label>
 
 						<div class="col-md-8">
 							<input type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="{{ trans('app.Domain login') }}">
-							{{-- @if ($errors->has('email'))
-								<span class="help-block">
-									<strong>{{ $errors->first('email') }}</strong>
-								</span>
-							@endif --}}
 						</div>
 					</div>
 
-					<div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+					<div class="form-group">
 						<label class="col-md-3 control-label">{{ trans('app.Password') }}</label>
 
 						<div class="col-md-8">
 							<input type="password" class="form-control" name="password" placeholder="{{ trans('app.Domain Password') }}">
-							{{-- @if ($errors->has('password'))
-								<span class="help-block">
-									<strong>{{ $errors->first('password') }}</strong>
-								</span>
-							@endif --}}
 						</div>
 					</div>
 
@@ -51,8 +41,6 @@
 							<button type="submit" class="btn btn-primary">
 								<i class="fa fa-fw fa-btn fa-sign-in"></i> {{ trans('app.Login') }}
 							</button>
-
-							{{-- <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a> --}}
 						</div>
 					</div>
 				</form>
