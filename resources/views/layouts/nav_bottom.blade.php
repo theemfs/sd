9054dfffbe7b3ecdfd36c062406c7d731af3cad7
theemfs <theemfs@gmail.com>
2016-09-07 00:28:23 +0800

    some fixes

--- resources/views/layouts/nav_bottom.blade.php
+++ resources/views/layouts/nav_bottom.blade.php
@@ -1,21 +1,28 @@
 @if (Auth::user())
 
 <nav class="navbar navbar-default navbar-fixed-bottom">
-	<div class="container text-center">
-		<ul class="nav navbar-nav">
-			<li>
-				<a href="mailto:anton@grandbaikal.ru?subject=About IT Helpdesk">
-					{{-- <i class="fa fa-fw fa-btn fa-copyright"></i> --}}
-					<i class="fa fa-fw fa-btn fa-at"></i>
-					По всем вопросам пишите <strong><ins>anton@grandbaikal.ru</ins></strong>
-					{{-- <span class="text-muted small text-center">Идея и разработка - Антон Хамаев</span> --}}
-				</a>
-			</li>
-		</ul>
-		{{-- <span class="text-muted small">&copy / &reg;</span>
-		<span class="text-muted small">@</p> --}}
-		{{-- <p class="text-muted small text-center">&copy Идея и разработка - Антон Хамаев</p> --}}
-		{{-- <p class="text-muted small text-center">По всем вопросам пишите <a href="mailto:">anton@grandbaikal.ru</a></p> --}}
+	<div class="container">
+		<div class="col-md-8 col-md-offset-2">
+
+			<ul class="nav navbar-nav">
+				<li>
+					<p class="navbar-text small">
+						<i class="fa fa-fw fa-btn fa-copyright"></i>
+						<span class="text-muted">Идея и разработка - Антон Хамаев</span>
+					</p>
+				</li>
+			</ul>
+
+			<ul class="nav navbar-nav navbar-right">
+				<li>
+					<p class="navbar-text small navbar-right">
+						<i class="fa fa-fw fa-btn fa-at"></i>
+						По всем вопросам пишите <a href="mailto:anton@grandbaikal.ru?subject=About IT Helpdesk">anton@grandbaikal.ru</a>
+					</p>
+				</li>
+			</ul>
+
+		</div>
 	</div>
 </nav>
 
