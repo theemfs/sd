147c9d107bd15f42ff6f90e703917872dadcec36
theemfs <theemfs@gmail.com>
2016-09-11 22:16:36 +0800

    some fixes

---
+++ database/migrations/2016_09_11_142800_create_articles.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticles extends Migration
+{
+
+	public function up()
+	{
+	}
+
+	public function down()
+	{
+	}
+
+}
